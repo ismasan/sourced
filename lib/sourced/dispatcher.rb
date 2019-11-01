ec9b471d00610731d8dbeed8331590a56fa556b1
Ismael Celis <ismaelct@gmail.com>
2019-11-01 16:48:20 +0000

    Dispatcher#call takes an optional runtime handler instance

--- lib/sourced/dispatcher.rb
+++ lib/sourced/dispatcher.rb
@@ -1,19 +1,26 @@
 module Sourced
+  class NullHandler
+    def self.call(*_args)
+      [nil, []]
+    end
+  end
+
   class Dispatcher
-    def initialize(repository: nil, event_store:, handler:, subscribers: Subscribers.new)
+    def initialize(repository: nil, event_store:, handler: NullHandler, subscribers: Subscribers.new)
       @repository = repository || AggregateRepo.new(event_store: event_store)
       @event_store = event_store
-      @handler = handler
+      @_handler = handler
       @subscribers = subscribers
     end
 
-    def call(cmd)
-      aggr, events = handler.call(cmd, repository: repository)
+    def call(cmd, handler: nil)
+      hndl = handler || _handler
+      aggr, events = hndl.call(cmd, repository: repository)
       subscribers.call(event_store.append(events))
       aggr
     end
 
     private
-    attr_reader :repository, :event_store, :handler, :subscribers
+    attr_reader :repository, :event_store, :_handler, :subscribers
   end
 end
