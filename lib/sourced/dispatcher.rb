b34e0a236d7fb3a1c3413dcf541cd3bb142ddc43
Ismael Celis <ismaelct@gmail.com>
2019-11-05 10:42:38 +0000

    Simplify CommandHandler

--- lib/sourced/dispatcher.rb
+++ lib/sourced/dispatcher.rb
@@ -16,7 +16,9 @@ module Sourced
     def call(cmd, handler: nil)
       hndl = handler || _handler
       validate_handler!(hndl, cmd)
-      aggr, events = hndl.call(cmd, repository: repository)
+      aggr = load_aggregate(cmd.aggregate_id, hndl.aggregate_class)
+      aggr, events = hndl.call(cmd, aggr)
+      events = collect_and_decorate_events(events, cmd)
       subscribers.call(event_store.append(events))
       aggr
     end
@@ -28,6 +30,23 @@ module Sourced
       if !handler.topics.include?(cmd.topic)
         raise UnhandledCommandError, "#{handler} does not handle command '#{cmd.topic}'"
       end
+    end
+
+    def load_aggregate(id, aggregate_class)
+      return nil unless aggregate_class
+      repository.load(id, aggregate_class)
+    end
+
+    def collect_and_decorate_events(events, cmd)
+      out = events.map do |evt|
+        evt.copy(copy_cmd_attrs(cmd))
+      end
+
+      [cmd] + out
+    end
+
+    def copy_cmd_attrs(cmd)
+      { parent_id: cmd.id }
     end
   end
 end
--- spec/command_handler_spec.rb
+++ spec/command_handler_spec.rb
@@ -7,22 +7,27 @@ RSpec.describe Sourced::CommandHandler do
     end
   end
 
-  describe '#call(command)' do
+  specify '#aggregate_class' do
+    expect(UserDomain::UserHandler.aggregate_class).to eq UserDomain::User
+  end
+
+  describe '#call(command, aggregate)' do
     it 'handles command and gathers events' do
       id = Sourced.uuid
+      user = UserDomain::User.new(id: id)
       cmd = UserDomain::CreateUser.new!(
         aggregate_id: id,
         name: 'Ismael',
         age: 40,
       )
-      user, events = UserDomain::UserHandler.call(cmd)
+      user, events = UserDomain::UserHandler.call(cmd, user)
       expect(user.id).to eq id
       expect(user.name).to eq 'Ismael'
-      expect(events.size).to eq 4
-      expect(events.map(&:topic)).to eq %w(users.create users.created users.name.changed users.age.changed)
+      expect(events.size).to eq 3
+      expect(events.map(&:topic)).to eq %w(users.created users.name.changed users.age.changed)
       expect(events.map(&:aggregate_id).uniq).to eq [id]
-      expect(events.map(&:version)).to eq [1, 1, 2, 3]
-      expect(events.map(&:parent_id).uniq).to eq [nil, cmd.id] # command itself doesn't have parent id
+      expect(events.map(&:version)).to eq [1, 2, 3]
+      # expect(events.map(&:parent_id).uniq).to eq [nil, cmd.id] # command itself doesn't have parent id
     end
   end
 
