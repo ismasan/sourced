32e07cf47e5805311c3fcd9de8277c6ce65c1a24
Ismael Celis <ismaelct@gmail.com>
2019-11-07 17:59:32 +0000

    First stab at self-persisting aggregates

---
+++ lib/sourced/persistable.rb
@@ -0,0 +1,26 @@
+module Sourced
+  module Persistable
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    def persist
+      self.class.repository.persist(self)
+    end
+
+    module ClassMethods
+      def repository(repo = nil)
+        @repository = repo if repo
+        @repository
+      end
+
+      def load(id)
+        repository.load(id, self)
+      end
+
+      def build
+        repository.build(self)
+      end
+    end
+  end
+end
