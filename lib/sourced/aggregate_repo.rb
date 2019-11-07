daac3f8c3108120f6c13e946f0cbf87d0f633cb4
Ismael Celis <ismaelct@gmail.com>
2019-11-07 17:14:32 +0000

    Simplify AggregateRepo. #build, #load, #persist

--- lib/sourced/aggregate_repo.rb
+++ lib/sourced/aggregate_repo.rb
@@ -2,54 +2,25 @@ module Sourced
   class AggregateRepo
     def initialize(event_store: MemEventStore.new)
       @event_store = event_store
-      reset!
-    end
-
-    def add(id, aggregate_class)
-      if aggr = aggregates[id]
-        aggr
-      else
-        register_aggregate(id, aggregate_class)
-      end
     end
 
     def load(id, aggregate_class, opts = {})
-      catchup = !!opts.delete(:catchup)
-      # if aggregate already cached, should we use that one and only load any new events?
-      if aggr = aggregates[id]
-        # catch up with new events, if any
-        if catchup
-          stream = event_store.by_aggregate_id(id, from: aggr.version)
-          aggr.load_from stream
-        end
-        aggr
-      else
-        stream = event_store.by_aggregate_id(id, opts)
-        aggr = register_aggregate(id, aggregate_class)
-        aggr.load_from(stream)
-        aggr
-      end
-    end
-
-    def clear_events
-      evts = events.dup
-      events.clear
-      evts
-    end
-
-    private
-    attr_reader :event_store, :aggregates, :events
-
-    def register_aggregate(id, aggregate_class)
-      aggr = aggregate_class.new(id, events: events)
-      raise InvalidAggregateError, 'aggregates must set :id on initialize' unless aggr.id == id
-      aggregates[id] = aggr
+      stream = event_store.by_aggregate_id(id, opts)
+      aggr = aggregate_class.new(id)
+      aggr.load_from(stream)
       aggr
     end
 
-    def reset!
-      @aggregates = {}
-      @events = []
+    def build(aggregate_class)
+      aggregate_class.new(id: Sourced.uuid)
     end
+
+    def persist(aggregate)
+      event_store.append(aggregate.clear_events)
+    end
+
+    private
+
+    attr_reader :event_store
   end
 end
--- spec/aggregate_repo_spec.rb
+++ spec/aggregate_repo_spec.rb
@@ -5,32 +5,11 @@ RSpec.describe Sourced::AggregateRepo do
   let(:aggregate_id) { Sourced.uuid }
   subject(:repo) { described_class.new(event_store: event_store) }
 
-  describe '#add' do
-    it 'registers aggregate without loading history' do
-      event_store.append([
-        UserDomain::UserCreated.new!(aggregate_id: aggregate_id, version: 1, name: 'Ismael', age: 39),
-        UserDomain::NameChanged.new!(aggregate_id: aggregate_id, version: 2, name: 'Mr. Ismael'),
-      ])
-
-      user = repo.add(aggregate_id, UserDomain::User)
-      expect(user.id).to eq aggregate_id
-      expect(user.name).to be nil
-      expect(user.version).to eq 0
-    end
-  end
-
   describe '#load' do
-    it 'loads new aggregate when no events available' do
-      user = repo.load(aggregate_id, UserDomain::User)
-      expect(user.id).to eq aggregate_id
-      # loads same new user again, even if it's new
-      user2 = repo.load(aggregate_id, UserDomain::User)
-      expect(user.object_id).to eq user.object_id
-    end
-
     it 'loads from available history' do
       event_store.append([
         UserDomain::UserCreated.new!(aggregate_id: aggregate_id, version: 1, name: 'Ismael', age: 39),
+        UserDomain::UserCreated.new!(aggregate_id: Sourced.uuid, version: 1, name: 'Someone Else', age: 20),
         UserDomain::NameChanged.new!(aggregate_id: aggregate_id, version: 2, name: 'Mr. Ismael'),
         UserDomain::AgeChanged.new!(aggregate_id: aggregate_id, version: 3, age: 40),
       ])
@@ -40,11 +19,6 @@ RSpec.describe Sourced::AggregateRepo do
       expect(user.name).to eq 'Mr. Ismael'
       expect(user.age).to eq 40
       expect(user.version).to eq 3
-
-      # it loads cached aggregate
-      expect(event_store).not_to receive(:by_aggregate_id)
-      user2 = repo.load(aggregate_id, UserDomain::User)
-      expect(user).to eq user2
     end
 
     it 'loads up to a specific version' do
@@ -61,54 +35,23 @@ RSpec.describe Sourced::AggregateRepo do
       expect(user.events.size).to eq 0
       expect(user.version).to eq 2
     end
+  end
 
-    it 'catches up with new changes if :catchup' do
-      event_store.append([
-        UserDomain::UserCreated.new!(aggregate_id: aggregate_id, version: 1, name: 'Ismael', age: 39),
-        UserDomain::NameChanged.new!(aggregate_id: aggregate_id, version: 2, name: 'Mr. Ismael'),
-      ])
-
-      user = repo.load(aggregate_id, UserDomain::User)
-      expect(user.id).to eq aggregate_id
-      expect(user.name).to eq 'Mr. Ismael'
-      expect(user.age).to eq 39
-      expect(user.version).to eq 2
-
-      event_store.append([
-        UserDomain::AgeChanged.new!(aggregate_id: aggregate_id, version: 3, age: 40),
-      ])
-
-      user2 = repo.load(aggregate_id, UserDomain::User, catchup: true)
-      expect(user2).to eq user
-      expect(user2.id).to eq aggregate_id
-      expect(user2.name).to eq 'Mr. Ismael'
-      expect(user2.age).to eq 40
-      expect(user2.version).to eq 3
+  describe '#build' do
+    it 'builds a new aggregate with a UUID' do
+      user = repo.build(UserDomain::User)
+      expect(user.id).not_to be_nil
     end
   end
 
-  describe '#clear_events' do
-    it 'collects aggregate events, in the order they were applied across aggregates' do
-      id1 = Sourced.uuid
-      id2 = Sourced.uuid
-      user1 = repo.load(id1, UserDomain::User)
-      user2 = repo.load(id2, UserDomain::User)
-
-      user1.apply UserDomain::UserCreated, aggregate_id: id1, name: 'Ismael', age: 39
-      user1.apply UserDomain::NameChanged, name: 'Ismael'
-      user2.apply UserDomain::UserCreated, aggregate_id: id2, name: 'Joe', age: 42
-      user1.apply UserDomain::AgeChanged, age: 40
-
-      # expect(user1.events.map(&:topic)).to eq %w(users.created users.name.changed users.age.changed)
-      # expect(user2.events.map(&:topic)).to eq %w(users.created)
-
-      events = repo.clear_events
-      # events will be in order aggregates where loaded, not in order they where applied to aggregates
-      # is this a problem?
-      # expect(events.map(&:topic)).to eq %w(users.created users.name.changed users.age.changed users.created)
-      expect(events.map(&:topic)).to eq %w(users.created users.name.changed users.created users.age.changed)
-      expect(events.map(&:aggregate_id)).to eq [user1.id, user1.id, user2.id, user1.id]
-      # expect(events.map(&:aggregate_id)).to eq [user1.id, user1.id, user1.id, user2.id]
+  describe '#persist' do
+    it 'persists aggregate events into the event store' do
+      user = repo.build(UserDomain::User)
+      user.start('Ismael', 41)
+      user.name = 'Joe'
+      repo.persist(user)
+      stream = event_store.by_aggregate_id(user.id)
+      expect(stream.map(&:topic)).to eq %w(users.created users.name.changed)
     end
   end
 end
