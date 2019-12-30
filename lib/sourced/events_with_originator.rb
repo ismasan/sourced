8198a270f9e43a39f76816ad121107cb1f04c09c
Ismael Celis <ismaelct@gmail.com>
2019-12-30 12:55:59 +0000

    EventsWithOriginator commits session events, decorated with command as originator

---
+++ lib/sourced/events_with_originator.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Sourced
+  class EventsWithOriginator
+    def initialize(originator, committable)
+      @originator = originator
+      @committable = committable
+    end
+
+    def commit(&_block)
+      evts = to_a
+      @committable.commit do |seq, _events|
+        yield seq, evts
+      end
+    end
+
+    def to_a
+      @to_a ||= (
+        [
+          @originator,
+          *@committable.events.map do |evt|
+            evt.copy(originator_id: @originator.id)
+          end
+        ]
+      )
+    end
+  end
+end
