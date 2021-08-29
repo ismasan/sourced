89a912f93d62cd3006acddbbe274833f710b7a33
Ismael Celis <ismaelct@gmail.com>
2021-08-29 14:08:25 +0100

    IdentityRepo#persist(stage, &tx_block)

--- lib/sourced/entity_repo.rb
+++ lib/sourced/entity_repo.rb
@@ -19,17 +19,18 @@ module Sourced
       stage_builder.load(id, stream)
     end
 
-    def persist(stage)
+    def persist(stage, &block)
       stage.commit do |last_committed_seq, events, entity|
         event_store.transaction do
           events = persist_events(events, expected_seq: last_committed_seq)
-          dispatch(events, entity)
+          block.call(entity, events) if block_given?
+          dispatch(entity, events)
         end
       end
     end
 
-    def persist_with_originator(stage, originator_event)
-      persist(CommitterWithOriginator.new(originator_event, stage))
+    def persist_with_originator(stage, originator_event, &block)
+      persist(CommitterWithOriginator.new(originator_event, stage), &block)
     end
 
     def persist_events(events, expected_seq: nil)
@@ -40,7 +41,7 @@ module Sourced
 
     attr_reader :stage_builder, :event_store, :subscribers
 
-    def dispatch(events, entity)
+    def dispatch(entity, events)
       # Subscribers should not be dependent on eachother,
       # so this could potentially write concurrently to all of them.
       subscribers.each do |sub|
