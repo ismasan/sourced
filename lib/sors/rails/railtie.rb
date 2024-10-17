7f6992f73d5ca8a15449c31d0df82d8d8cc68958
Ismael Celis <ismaelct@gmail.com>
2024-10-17 21:00:09 +0100

    Comment

--- lib/sors/rails/railtie.rb
+++ lib/sors/rails/railtie.rb
@@ -3,6 +3,11 @@
 module Sors
   module Rails
     class Railtie < ::Rails::Railtie
+      # TODO: review this.
+      # Workers use Async, so this is needed
+      # but not sure this can be safely used with non Async servers like Puma.
+      # config.active_support.isolation_level = :fiber
+
       generators do
         require 'sors/rails/install_generator'
       end
