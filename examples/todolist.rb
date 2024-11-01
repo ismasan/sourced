9ddb44bdcb76bea6eb4d548c4caae8c24732512d
Ismael Celis <ismaelct@gmail.com>
2024-11-01 23:34:49 +0000

    WiP CartListings projector

--- examples/todolist.rb
+++ examples/todolist.rb
@@ -61,6 +61,7 @@ class Cart < Sors::Aggregate
 
   # Defines a Cart::Notify command struct
   command :notify, 'cart.notify', mailer_id: String do |cmd|
+    puts "#{self.class.name} #{cmd.stream_id} NOTIFY"
     cmd.follow(Notified, mailer_id: cmd.payload.mailer_id)
   end
 
@@ -122,6 +123,71 @@ class CartEmailsSaga < Sors::Machine
   end
 end
 
+# A projector
+# "reacts" to events registered with .evolve
+class CartListings < Sors::Aggregate
+  class << self
+    def handled_events = self.handled_events_for_evolve
+
+    # The Reactor interface
+    # @param events [Array<Message>]
+    def handle_events(events)
+      # For this type of event sourced projections
+      # that load current state from events
+      # then apply "new" events
+      # TODO: the current state already includes
+      # the new events, so we need to load upto events.first.seq
+      puts "LOAD UPTO #{self.name} #{events.first.stream_id} #{events.first.seq - 1}"
+      instance = load(events.first.stream_id, upto: events.first.seq - 1)
+      puts "HANDLE #{self.name} #{events.first.stream_id} #{events.first.seq}"
+      instance.handle_events(events)
+    end
+  end
+
+  def handle_events(events, &map_commands)
+    evolve(events)
+    save
+    [] # no commands
+  end
+
+  def setup(id)
+    @cart = { id:, items: [], status: :open, seq: 0, seqs: [] }
+    FileUtils.mkdir_p('examples/carts')
+  end
+
+  def save
+    File.write("./examples/carts/#{@cart[:id]}.json", JSON.pretty_generate(@cart))
+  end
+
+  # This is wrong.
+  # :any means this reactor will get ANY events from ANY AGGREGATE
+  # ie. Cart, Mailer
+  # Instead we want something like
+  # evolve_any_from Cart
+  # or
+  # evolve_any_from 'cart'
+  # evolve :any do |event|
+  #   puts "ANY EVENT: #{event.stream_id} #{event.type} #{event.seq}"
+  #   @cart[:seq] = event.seq
+  #   @cart[:seqs] << event.seq
+  # end
+
+  evolve Cart::Placed do |event|
+    @cart[:status] = :placed
+  end
+
+  evolve Cart::ItemAdded do |event|
+    @cart[:items] << event.payload.to_h
+  end
+
+  evolve Cart::Notified do |event|
+    @cart[:notified] = true
+  end
+end
+
+# Register the Cart Aggregate with the Router
+# This allows the Router to route commands to the Cart Aggregate
 Sors::Router.register(Cart)
 Sors::Router.register(Mailer)
 Sors::Router.register(CartEmailsSaga)
+Sors::Router.register(CartListings)
