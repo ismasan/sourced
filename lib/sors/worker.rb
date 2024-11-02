e5d5c4290d02e639ab66da4fb8c5857bd36d6989
Ismael Celis <ismaelct@gmail.com>
2024-11-02 16:42:18 +0000

    Big comment on inline command processing breaking per-stream concurrency model

--- lib/sors/worker.rb
+++ lib/sors/worker.rb
@@ -75,6 +75,21 @@ module Sors
           # This will run a new decider
           # which may be expensive, timeout, or raise an exception
           # TODO: handle decider errors
+          # TODO2: this breaks the per-stream concurrency model.
+          # Ex. if the current event belongs to a 'cart1' stream,
+          # the DB is locked from processing any new events for 'cart1'
+          # until we exist this block.
+          # if ex. reactor is a Saga that produces a command for another stream
+          # (ex. 'mailer-1'), by processing the command here inline, we're blocking
+          # the 'cart1' stream unnecessarily
+          # Instead, we can:
+          # if command.stream_id == event.stream_id
+          # * it's Ok to block, as we keep per-stream concurrency
+          # if command.stream_id != event.stream_id
+          # * we should schedule the command to be picked up later.
+          # We can't just run the command in a separate Fiber.
+          # We want the durability of a command bus.
+          # A command bus will also solve future and recurrent scheduled commands.
           log_event(' -> produced command', reactor, commands.first)
           Sors::Router.handle(commands.first)
         end
