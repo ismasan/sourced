3d265ce703086bddd07c4ea4d2ea485d5ec3803e
Ismael Celis <ismaelct@gmail.com>
2024-11-13 15:40:13 +0000

    Clear all files

--- spec/mem_event_store_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe Sourced::MemEventStore do
-  it_behaves_like 'a Sourced event store' do
-    subject(:event_store) { described_class.new }
-  end
-end
