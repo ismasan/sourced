d05441b1f8c834e5f6488bc6e73718687ba5dec9
Ismael Celis <ismaelct@gmail.com>
2024-11-13 17:42:54 +0000

    Consumer.consumer_info.start_from
    can be an integer (seq), a Symbol (:now, :beginning), a #call() => Time

--- lib/sourced/consumer.rb
+++ lib/sourced/consumer.rb
@@ -2,10 +2,32 @@
 
 module Sourced
   module Consumer
-    ConsumerInfo = Data.define(:group_id)
+    class ConsumerInfo < Types::Data
+      ToBlock = Types::Any.transform(Proc) { |v| -> { v } }
+      StartFromSymbols = Types::Symbol.options(%i[beginning now]) >> ToBlock
+      StartFromTime = Types::Interface[:call].check('must return a Time') { |v| v.call.is_a?(Time) }
+      StartFromSequence = Types::Integer >> ToBlock
+
+      StartFrom = (
+        StartFromSymbols | StartFromTime | StartFromSequence
+      ).default { -> { :beginning } }
+
+      attribute :group_id, Types::String.present, writer: true
+      attribute :start_from, StartFrom, writer: true
+    end
 
     def consumer_info
-      ConsumerInfo.new(group_id: name)
+      @consumer_info ||= ConsumerInfo.new(group_id: name)
+    end
+
+    def consumer(&)
+      return consumer_info unless block_given?
+
+      info = ConsumerInfo.new(group_id: name)
+      yield info
+      raise Plumb::ParseError, info.errors unless info.valid?
+
+      @consumer_info = info
     end
   end
 end
