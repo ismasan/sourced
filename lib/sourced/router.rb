b56cbea7bde44c78abeb41653f00300453920698
Ismael Celis <ismaelct@gmail.com>
2024-12-02 12:58:00 +0000

    Instance method Router#handle_and_ack_events_for_reactor

--- lib/sourced/router.rb
+++ lib/sourced/router.rb
@@ -25,35 +25,15 @@ module Sourced
         instance.async_reactors
       end
 
-      # When in sync mode, we want both events
-      # and any resulting commands to be processed syncronously
-      # and in the same transaction as events are appended to store.
-      # We could handle commands in threads or fibers,
-      # if they belong to different streams than the events,
-      # but we need to make sure to raise exceptions in the main thread.
-      # so that the transaction is rolled back.
       def handle_and_ack_events_for_reactor(reactor, events)
-        Sourced.config.backend.ack_on(reactor.consumer_info.group_id, events.last.id) do
-          commands = reactor.handle_events(events)
-          if commands && commands.any?
-            # TODO: Commands may or may not belong to he same stream as events
-            # if they belong to the same stream,
-            # hey need to be dispached in order to preserve per stream order
-            # If they belong to different streams, they can be dispatched in parallel
-            # or put in a command bus.
-            # TODO2: we also need to handle exceptions here
-            # TODO3: this is not tested
-            commands.each do |cmd|
-              Sourced::Router.handle_command(cmd)
-            end
-          end
-        end
+        instance.handle_and_ack_events_for_reactor(reactor, events)
       end
     end
 
-    attr_reader :sync_reactors, :async_reactors
+    attr_reader :sync_reactors, :async_reactors, :backend
 
-    def initialize
+    def initialize(backend: Sourced.config.backend)
+      @backend = backend
       @decider_lookup = {}
       @sync_reactors = Set.new
       @async_reactors = Set.new
@@ -90,7 +70,32 @@ module Sourced
       # I need to think about how to best to handle this safely
       # Also this could potential lead to infinite recursion!
       reactors.each do |r|
-        Router.handle_and_ack_events_for_reactor(r, events)
+        handle_and_ack_events_for_reactor(r, events)
+      end
+    end
+
+    # When in sync mode, we want both events
+    # and any resulting commands to be processed syncronously
+    # and in the same transaction as events are appended to store.
+    # We could handle commands in threads or fibers,
+    # if they belong to different streams than the events,
+    # but we need to make sure to raise exceptions in the main thread.
+    # so that the transaction is rolled back.
+    def handle_and_ack_events_for_reactor(reactor, events)
+      backend.ack_on(reactor.consumer_info.group_id, events.last.id) do
+        commands = reactor.handle_events(events)
+        if commands && commands.any?
+          # TODO: Commands may or may not belong to he same stream as events
+          # if they belong to the same stream,
+          # hey need to be dispached in order to preserve per stream order
+          # If they belong to different streams, they can be dispatched in parallel
+          # or put in a command bus.
+          # TODO2: we also need to handle exceptions here
+          # TODO3: this is not tested
+          commands.each do |cmd|
+            handle_command(cmd)
+          end
+        end
       end
     end
   end
