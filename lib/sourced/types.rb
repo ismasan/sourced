0953a8cf7ae13aa751c763d93e290af317a4c86e
Ismael Celis <ismaelct@gmail.com>
2024-12-02 17:01:44 +0000

    not using serializable time

--- lib/sourced/types.rb
+++ lib/sourced/types.rb
@@ -10,18 +10,5 @@ module Sourced
 
     # A UUID string, or generate a new one
     AutoUUID = UUID::V4.default { SecureRandom.uuid }
-
-    module JSON
-      class SerializableTime < ::Time
-        def to_json(*)
-          "\"#{strftime('%Y-%m-%dT%H:%M:%S.%N%z')}\""
-        end
-      end
-
-      UTCTime = Any[SerializableTime] |
-                (String >> Any.build(SerializableTime, :parse).policy(:rescue, ::ArgumentError)).invoke(:utc)
-
-      AutoUTCTime = UTCTime.default { SerializableTime.new.utc }
-    end
   end
 end
