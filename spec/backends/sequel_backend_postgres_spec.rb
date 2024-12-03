9d78991213e00ffe02c0ce2d16f308608f8a3bcc
Ismael Celis <ismaelct@gmail.com>
2024-12-03 12:08:42 +0000

    Message#metadata and PG column

--- spec/backends/sequel_backend_postgres_spec.rb
+++ spec/backends/sequel_backend_postgres_spec.rb
@@ -7,15 +7,12 @@ RSpec.describe 'Sourced::Backends::SequelBackend with Postgres', type: :backend 
   subject(:backend) { Sourced::Backends::SequelBackend.new(db) }
 
   let(:db) do
-    Sequel.postgres('sors_test')
+    Sequel.postgres('sourced_test')
   end
 
   before do
-    backend.install unless backend.installed?
-  end
-
-  after do
-    backend.clear!
+    backend.clear! if backend.installed?
+    backend.install
   end
 
   it_behaves_like 'a backend'
