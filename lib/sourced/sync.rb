84159fdd7cd6adad21f39d51528707d8c2532bc5
Ismael Celis <ismaelct@gmail.com>
2024-12-10 11:40:24 +0000

    Document Sync

--- lib/sourced/sync.rb
+++ lib/sourced/sync.rb
@@ -1,12 +1,44 @@
 # frozen_string_literal: true
 
 module Sourced
+  # This mixin provides a .sync macro to registering blocks
+  # that will run within a transaction. Ie. in Deciders when appending events to the backend,
+  # or projectors when persisting their state.
+  # @example
+  #
+  #  class CartDecider < Sourced::Decider
+  #    # Run this block within a transaction
+  #    # when appending messages to storage
+  #    sync do |state, command, events|
+  #      # Do something here, like updating a view, sending an email, etc.
+  #    end
+  #  end
+  #
+  # When given a ReactorInterface, it will run that reactor synchronously
+  # and ACK the offsets for the embedded reactor and consumed events.
+  # This is so that reactors can be run in a strong consistency manner
+  # within a Decider lifecycle.
+  # ACKing the events will ensure that the events are not reprocessed
+  # if the child reactor is later moved to eventually consistent processing.
+  # @example
+  #
+  #  class CartDecider < Sourced::Decider
+  #    # The CartListings projector
+  #    # will be run synchronously when events are appended by this Decider
+  #    # Any error raised by the projector will cause the transaction to rollback
+  #    sync CartListings
+  #  end
   module Sync
     def self.included(base)
       super
       base.extend ClassMethods
     end
 
+    # Host classes will call this method to run any registered .sync blocks
+    # within their transactional boundaries.
+    # @param state [Object] the current state of the host class
+    # @param command [Object, Nil] the command being processed
+    # @param events [Array<Sourced::Event>] the events being appended
     def run_sync_blocks(state, command, events)
       self.class.sync_blocks.each do |blk|
         case blk
@@ -24,6 +56,8 @@ module Sourced
 
     CallableInterface = Sourced::Types::Interface[:call]
 
+    # Wrap a sync reactor and call it's handle_events method
+    # while also ACKing its offsets for the processed events.
     class SyncReactor < SimpleDelegator
       def handle_events(events)
         Router.handle_and_ack_events_for_reactor(__getobj__, events)
@@ -46,6 +80,19 @@ module Sourced
         @sync_blocks ||= []
       end
 
+      # The .sync macro
+      # @example
+      #
+      #  sync do |state, command, events|
+      #    # Do something here, like updating a view, sending an email, etc.
+      #  end
+      #
+      #  sync CartListings
+      #
+      # @param callable [Nil, Proc, ReactorInterface, CallableInterface] the block to run
+      # @yieldparam state [Object] the state of the host class
+      # @yieldparam command [Object, Nil] the command being processed
+      # @yieldparam events [Array<Sourced::Event>] the events being appended
       def sync(callable = nil, &block)
         callable ||= block
         callable = case callable
@@ -57,15 +104,6 @@ module Sourced
 
                      callable
                    when ReactorInterface
-                     # Wrap reactors here
-                     # TODO:
-                     # If the sync reactor runs successfully
-                     # A). we want to ACK processed events for it in the offsets table
-                     # so that if the reactor is moved to async execution
-                     # it doesn't reprocess the same events again
-                     # B). The reactors .handle_events may return commands
-                     # Do we want to dispatch those commands inline?
-                     # Or is this another reason to have a separate async command bus
                      SyncReactor.new(callable)
                    when CallableInterface
                      callable
