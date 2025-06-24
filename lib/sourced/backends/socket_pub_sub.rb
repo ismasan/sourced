86e61e3eca3c505265c5745e7859da5585c572c4
Ismael Celis <ismaelct@gmail.com>
2025-06-24 12:28:09 +0100

    Working SocketPubSub with broadcast to multiple connected clients

--- lib/sourced/backends/socket_pub_sub.rb
+++ lib/sourced/backends/socket_pub_sub.rb
@@ -44,19 +44,24 @@ module Sourced
       # @param event [Sourced::Message]
       # @return [self]
       def publish(channel_name, event)
-        socket_path = File.join(@socket_dir, "#{channel_name}.sock")
-        
-        # Only publish if there are subscribers (socket exists)
-        return self unless File.exist?(socket_path)
+        channel_dir = File.join(@socket_dir, channel_name)
+        return self unless Dir.exist?(channel_dir)
 
-        begin
-          UNIXSocket.open(socket_path) do |socket|
-            event_data = JSON.dump(event.to_h)
-            socket.write("#{event_data}\n")
+        event_data = JSON.dump(event.to_h)
+        
+        # Find all subscriber socket files for this channel
+        subscriber_sockets = Dir.glob(File.join(channel_dir, "*.sock"))
+        
+        subscriber_sockets.each do |socket_path|
+          begin
+            UNIXSocket.open(socket_path) do |socket|
+              socket.write("#{event_data}\n")
+            end
+          rescue Errno::ECONNREFUSED, Errno::ENOENT
+            # Subscriber socket exists but no one is listening
+            # Clean up stale socket files
+            File.unlink(socket_path) rescue nil
           end
-        rescue Errno::ECONNREFUSED, Errno::ENOENT
-          # Socket exists but no one is listening, or socket was removed
-          # This is expected behavior - messages are dropped if no subscribers
         end
 
         self
@@ -65,103 +70,109 @@ module Sourced
       private
 
       attr_reader :socket_dir
-    end
 
-    class SocketChannel
-      attr_reader :name, :socket_path
+      class Channel
+        attr_reader :name, :socket_path
 
-      # @param socket_dir [String] Directory containing socket files
-      # @param name [String] Channel name
-      def initialize(socket_dir:, name:)
-        @socket_dir = socket_dir
-        @name = name
-        @socket_path = File.join(socket_dir, "#{name}.sock")
-        @running = false
-        @server = nil
-        @mutex = Mutex.new
-      end
-
-      # Start listening to incoming events via Unix socket
-      #
-      # @param handler [#call, nil] a callable object to use as an event handler
-      # @yieldparam [Sourced::Message] The received event
-      # @yieldparam [SocketChannel] This channel instance
-      # @return [self]
-      def start(handler: nil, &block)
-        @mutex.synchronize do
-          return self if @running
-
-          handler ||= block
-          @running = true
-
-          # Clean up any existing socket file
-          File.unlink(@socket_path) if File.exist?(@socket_path)
-
-          @server = UNIXServer.new(@socket_path)
+        # @param socket_dir [String] Directory containing socket files
+        # @param name [String] Channel name
+        def initialize(socket_dir:, name:)
+          @socket_dir = socket_dir
+          @name = name
+          @channel_dir = File.join(socket_dir, name)
           
-          # Start accepting connections in a separate thread
-          @listener_thread = Thread.new do
-            begin
-              while @running
-                begin
-                  # Accept connections with a timeout to check @running periodically
-                  if IO.select([@server], nil, nil, 1)
-                    client = @server.accept
-                    handle_client(client, handler)
+          # Create unique socket file for this subscriber
+          # Using process ID, thread ID, and timestamp to ensure uniqueness
+          subscriber_id = "#{Process.pid}_#{Thread.current.object_id}_#{Time.now.to_f}"
+          @socket_path = File.join(@channel_dir, "#{subscriber_id}.sock")
+          
+          @running = false
+          @server = nil
+          @listener_thread = nil
+          @mutex = Mutex.new
+          @handler = nil
+        end
+
+        # Start listening to incoming events via Unix socket
+        #
+        # @param handler [#call, nil] a callable object to use as an event handler
+        # @yieldparam [Sourced::Message] The received event
+        # @yieldparam [SocketChannel] This channel instance
+        # @return [self]
+        def start(handler: nil, &block)
+          @mutex.synchronize do
+            return self if @running
+
+            @handler = handler || block
+            @running = true
+
+            # Create channel directory if it doesn't exist
+            FileUtils.mkdir_p(@channel_dir)
+
+            # Create unique socket file for this subscriber
+            @server = UNIXServer.new(@socket_path)
+            
+            # Start accepting connections in a separate thread
+            @listener_thread = Thread.new do
+              begin
+                while @running
+                  begin
+                    # Accept connections with a timeout to check @running periodically
+                    if IO.select([@server], nil, nil, 1)
+                      client = @server.accept
+                      handle_client(client)
+                    end
+                  rescue => e
+                    puts "Error in socket listener: #{e.message}" if $DEBUG
                   end
-                rescue => e
-                  # Log error but continue listening
-                  puts "Error in socket listener: #{e.message}" if $DEBUG
                 end
+              ensure
+                cleanup
               end
-            ensure
-              cleanup
             end
           end
+
+          @listener_thread.join
+          self
         end
 
-        self
-      end
-
-      # Stop listening and clean up resources
-      def stop
-        @mutex.synchronize do
-          @running = false
-          @listener_thread&.join(5) # Wait up to 5 seconds for thread to finish
-          cleanup
-        end
-      end
-
-      # Public so that we can test this separately from async channel listening
-      def parse(payload)
-        data = JSON.parse(payload.strip, symbolize_names: true)
-        Sourced::Message.from(data)
-      end
-
-      private
-
-      def handle_client(client, handler)
-        begin
-          while @running && (line = client.gets)
-            event = parse(line)
-            handler.call(event, self)
+        # Stop listening and clean up resources
+        def stop
+          @mutex.synchronize do
+            @running = false
+            @listener_thread&.join(5) # Wait up to 5 seconds for thread to finish
+            cleanup
           end
-        rescue => e
-          puts "Error handling client: #{e.message}" if $DEBUG
-        ensure
-          client.close rescue nil
         end
-      end
 
-      def cleanup
-        @server&.close rescue nil
-        File.unlink(@socket_path) if File.exist?(@socket_path)
-        @server = nil
-        @listener_thread = nil
+        # Public so that we can test this separately from async channel listening
+        def parse(payload)
+          data = JSON.parse(payload.strip, symbolize_names: true)
+          Sourced::Message.from(data)
+        end
+
+        private
+
+        def handle_client(client)
+          begin
+            while @running && (line = client.gets)
+              event = parse(line)
+              @handler.call(event, self)
+            end
+          rescue => e
+            puts "Error handling client: #{e.message}" if $DEBUG
+          ensure
+            client.close rescue nil
+          end
+        end
+
+        def cleanup
+          @server&.close rescue nil
+          File.unlink(@socket_path) if File.exist?(@socket_path)
+          @server = nil
+          @listener_thread = nil
+        end
       end
     end
-
-    # Alias for backwards compatibility
-    Channel = SocketChannel
   end
 end
