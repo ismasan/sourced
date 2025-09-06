a2f7887b5eb161f542f6e38e96093b5e6a6b4e5e
Ismael Celis <ismaelct@gmail.com>
2025-09-06 18:11:48 +0100

    Make Backend#append_next_to_stream support an array of events

--- spec/shared_examples/backend_examples.rb
+++ spec/shared_examples/backend_examples.rb
@@ -248,11 +248,41 @@ module BackendExamples
     end
 
     describe '#append_next_to_stream' do
-      it 'appends event to stream incrementing :seq automatically' do
+      it 'appends single event to stream incrementing :seq automatically' do
         evt1 = Tests::SomethingHappened1.parse(stream_id: 's1', seq: 1, payload: { account_id: 1 })
         backend.append_to_stream('s1', [evt1])
         evt2 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 2 })
         expect(backend.append_next_to_stream('s1', evt2)).to be(true)
+        events = backend.read_event_stream('s1')
+        expect(events.map(&:stream_id)).to eq(['s1', 's1'])
+        expect(events.map(&:seq)).to eq([1, 2])
+      end
+
+      it 'appends multiple events to stream incrementing :seq automatically' do
+        evt1 = Tests::SomethingHappened1.parse(stream_id: 's1', seq: 1, payload: { account_id: 1 })
+        backend.append_to_stream('s1', [evt1])
+        
+        evt2 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 2 })
+        evt3 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 3 })
+        evt4 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 4 })
+        
+        expect(backend.append_next_to_stream('s1', [evt2, evt3, evt4])).to be(true)
+        events = backend.read_event_stream('s1')
+        expect(events.map(&:stream_id)).to eq(['s1', 's1', 's1', 's1'])
+        expect(events.map(&:seq)).to eq([1, 2, 3, 4])
+      end
+
+      it 'handles empty array' do
+        expect(backend.append_next_to_stream('s1', [])).to be(true)
+        events = backend.read_event_stream('s1')
+        expect(events).to eq([])
+      end
+
+      it 'creates new stream when appending to non-existent stream with array' do
+        evt1 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 1 })
+        evt2 = Tests::SomethingHappened1.parse(stream_id: 's1', payload: { account_id: 2 })
+        
+        expect(backend.append_next_to_stream('s1', [evt1, evt2])).to be(true)
         events = backend.read_event_stream('s1')
         expect(events.map(&:stream_id)).to eq(['s1', 's1'])
         expect(events.map(&:seq)).to eq([1, 2])
