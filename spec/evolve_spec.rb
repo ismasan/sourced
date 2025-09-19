e8238c35980f9883a13cfaef51e0230217b42a6d
Ismael Celis <ismaelct@gmail.com>
2025-09-19 20:43:46 +0100

    Make .state macro part of Evolve, and rename .handled_events_for_evolve to .handled_messages_for_evolve
    Also remove id from state() factory.
    Actors can take #stream_id from messages.

--- spec/evolve_spec.rb
+++ spec/evolve_spec.rb
@@ -10,6 +10,10 @@ module EvolveTest
     Event2 = Sourced::Message.define('evolvetest.reactor.event2')
     Event3 = Sourced::Message.define('evolvetest.reactor.event3')
 
+    state do
+      []
+    end
+
     event Event1 do |state, event|
       state << event
     end
@@ -19,14 +23,26 @@ module EvolveTest
     end
   end
 
+  class ChildReactor < Reactor
+    event Event3
+  end
+
   class Noop
     include Sourced::Evolve
+
+    state do
+      []
+    end
 
     event Reactor::Event1
   end
 
   class EvolveAll
     include Sourced::Evolve
+
+    state do
+      []
+    end
 
     evolve_all Reactor do |state, event|
       state << event
@@ -44,33 +60,39 @@ RSpec.describe Sourced::Evolve do
   specify '.evolve' do
     evt1 = EvolveTest::Reactor::Event1.new(stream_id: '1', seq: 1)
     evt2 = EvolveTest::Reactor::Event2.new(stream_id: '1', seq: 2)
-    state = []
-    new_state = EvolveTest::Reactor.new.evolve(state.dup, [evt1, evt2])
-    expect(new_state).to eq([evt1, evt2])
+    state = EvolveTest::Reactor.new.evolve([evt1, evt2])
+    expect(state).to eq([evt1, evt2])
   end
 
-  specify '.handled_events_for_evolve' do
-    expect(EvolveTest::Reactor.handled_events_for_evolve).to eq([
-                                                                  EvolveTest::Reactor::Event1,
-                                                                  EvolveTest::Reactor::Event2
-                                                                ])
+  specify '.handled_messages_for_evolve' do
+    expect(EvolveTest::Reactor.handled_messages_for_evolve).to eq([
+      EvolveTest::Reactor::Event1,
+      EvolveTest::Reactor::Event2
+    ])
+
+    expect(EvolveTest::ChildReactor.handled_messages_for_evolve).to eq([
+      EvolveTest::Reactor::Event1,
+      EvolveTest::Reactor::Event2,
+      EvolveTest::Reactor::Event3,
+    ])
   end
 
   specify '.evolve handlers without a block' do
-    expect(EvolveTest::Noop.handled_events_for_evolve).to eq([EvolveTest::Reactor::Event1])
+    expect(EvolveTest::Noop.handled_messages_for_evolve).to eq([EvolveTest::Reactor::Event1])
 
     evt1 = EvolveTest::Reactor::Event1.new(stream_id: '1', seq: 1)
-    state = []
-    new_state = EvolveTest::Noop.new.evolve(state, [evt1])
-    expect(new_state).to eq(state)
+    new_state = EvolveTest::Noop.new.evolve([evt1])
+    expect(new_state).to eq([])
   end
 
   specify '.evolve_all' do
     evt1 = EvolveTest::Reactor::Event1.new(stream_id: '1', seq: 1)
     evt2 = EvolveTest::Reactor::Event2.new(stream_id: '1', seq: 2)
-    state = []
-    new_state = EvolveTest::EvolveAll.new.evolve(state.dup, [evt1, evt2])
+    evolver = EvolveTest::EvolveAll.new
+    expect(evolver.state).to eq([])
+    new_state = evolver.evolve([evt1, evt2])
     expect(new_state).to eq([evt1, evt2])
+    expect(evolver.state).to eq([evt1, evt2])
   end
 
   specify '.before_evolve' do
@@ -78,8 +100,7 @@ RSpec.describe Sourced::Evolve do
     evt2 = EvolveTest::Reactor::Event2.new(stream_id: '1', seq: 2)
     # evt3 is not handled by the reactor
     evt3 = EvolveTest::Reactor::Event3.new(stream_id: '1', seq: 3)
-    state = []
-    new_state = EvolveTest::WithBefore.new.evolve(state.dup, [evt1, evt2, evt3])
+    new_state = EvolveTest::WithBefore.new.evolve([evt1, evt2, evt3])
     expect(new_state).to eq([1, evt1, 2, evt2])
   end
 end
