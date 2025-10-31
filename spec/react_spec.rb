dfccf03aee301bdf17652bb6ea48db16816e7ee2
Ismael Celis <ismaelct@gmail.com>
2025-10-31 15:19:06 +0000

    React#dispatch

--- spec/react_spec.rb
+++ spec/react_spec.rb
@@ -17,16 +17,14 @@ class ReactTestReactor
   def state = { name: 'test' }
 
   reaction Event1 do |state, event|
-    stream_for(event).command Cmd1, name: state[:name]
+    dispatch(Cmd1, name: state[:name]).to(event)
   end
 
   reaction Event2 do |_state, event|
-    stream = stream_for(event)
-
-    stream.command Cmd2
-    stream.command Cmd3 do |cmd|
-      cmd.with_metadata(greeting: 'Hi!')
-    end
+    dispatch(Cmd2)
+    dispatch(Cmd3)
+      .with_metadata(greeting: 'Hi!')
+      .at(Time.now + 10)
   end
 
   reaction Event3 do |_state, _event|
@@ -35,20 +33,25 @@ class ReactTestReactor
 end
 
 RSpec.describe Sourced::React do
-  specify '#react returns AppendNext action with commands' do
-    evt1 = ReactTestReactor::Event1.new(stream_id: '1', seq: 1)
-    evt2 = ReactTestReactor::Event2.new(stream_id: '1', seq: 2)
-    commands = ReactTestReactor.new.react([evt1, evt2])
-    expect(commands.map(&:class)).to eq([
-      ReactTestReactor::Cmd1,
-      ReactTestReactor::Cmd2,
-      ReactTestReactor::Cmd3
-    ])
-    expect(commands.map { |e| e.metadata[:producer] }).to eq(%w[ReactTestReactor ReactTestReactor ReactTestReactor])
-    expect(commands.first.causation_id).to eq(evt1.id)
-    expect(commands.first.payload.name).to eq('test')
-    expect(commands.last.causation_id).to eq(evt2.id)
-    expect(commands.last.metadata[:greeting]).to eq('Hi!')
+  specify '#react returns messages to append or schedule' do
+    now = Time.now
+    Timecop.freeze(now) do
+      evt1 = ReactTestReactor::Event1.new(stream_id: '1', seq: 1)
+      evt2 = ReactTestReactor::Event2.new(stream_id: '1', seq: 2)
+      commands = ReactTestReactor.new.react([evt1, evt2])
+      expect(commands.map(&:class)).to eq([
+        ReactTestReactor::Cmd1,
+        ReactTestReactor::Cmd2,
+        ReactTestReactor::Cmd3
+      ])
+      expect(commands.map { |e| e.metadata[:producer] }).to eq(%w[ReactTestReactor ReactTestReactor ReactTestReactor])
+      expect(commands.first.causation_id).to eq(evt1.id)
+      expect(commands.first.created_at).to eq(now)
+      expect(commands.first.payload.name).to eq('test')
+      expect(commands.last.causation_id).to eq(evt2.id)
+      expect(commands.last.metadata[:greeting]).to eq('Hi!')
+      expect(commands.last.created_at).to eq(now + 10)
+    end
   end
 
   specify '.handled_messages_for_react' do
