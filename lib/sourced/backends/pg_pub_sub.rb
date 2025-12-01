49e0642a43c6c5090d687d13ac2263e162115b44
Ismael Celis <ismaelct@gmail.com>
2025-12-01 11:16:15 +0000

    Document PGPubSub. Wrap logging in blocks to avoid unnecessary string building.

--- lib/sourced/backends/pg_pub_sub.rb
+++ lib/sourced/backends/pg_pub_sub.rb
@@ -7,22 +7,49 @@ require 'json'
 module Sourced
   module Backends
     # a PubSub implementation using Postgres' LISTEN/NOTIFY
-    # @example
     #
-    #   # Publisher
-    #   Sourced.config.backend.pub_sub.publish('my_channel', event)
+    # This class provides a publish-subscribe mechanism using Postgres LISTEN/NOTIFY,
+    # with connection pooling - each channel name reuses a single database
+    # listener per process, allowing multiple subscribers to share the same listener thread / fiber.
+    # Relies on Sourced's configured Executor to use Threads or Fibers for concurrency
     #
-    #   # Subscriber
-    #   channel = Source.config.backend.pub_sub.subscribe('my_channel')
+    # @example Publishing a message
+    #   backend = Sourced.config.backend
+    #   event = MyEvent.new(stream_id: '111', payload: 'hello')
+    #   backend.pub_sub.publish('my_channel', event)
+    #
+    # @example Subscribing to messages
+    #   backend = Sourced.config.backend
+    #   channel = backend.pub_sub.subscribe('my_channel')
     #   channel.start do |event, ch|
     #     case event
     #     when MyEvent
-    #       # do something
+    #       puts "Received: #{event}"
     #     end
     #   end
     #
+    # @example Multiple subscribers to the same channel
+    #   # Both threads listen to the same channel but receive all messages
+    #   Thread.new do
+    #     ch1 = backend.pub_sub.subscribe('events')
+    #     ch1.start { |event| puts "Subscriber 1: #{event}" }
+    #   end
+    #
+    #   Thread.new do
+    #     ch2 = backend.pub_sub.subscribe('events')
+    #     ch2.start { |event| puts "Subscriber 2: #{event}" }
+    #   end
+    #
     class PGPubSub
+      # Listener holds a single DB connection per channel name/process
+      # Channel instances with the same name instantiated in different threads/fibers but the same process
+      # share a Listener instance
+      # The Listener receives messages and dispatches them to all channels.
       class Listener
+        # @param db [Sequel::Database] the database connection for listening
+        # @param channel_name [String] the name of the Postgres channel to listen on
+        # @param timeout [Integer] the timeout in seconds for listen operations (default: 2)
+        # @param logger [Logger]
         def initialize(db:, channel_name:, timeout: 2, logger:)
           @db = db
           @channel_name = channel_name
@@ -34,17 +61,28 @@ module Sourced
           @info = "[#{[self.class.name, @channel_name, Process.pid, object_id].join(' ')}]"
         end
 
+        # Subscribe a channel to this listener
+        #
+        # @param channel [Channel] the channel to subscribe
+        # @return [self]
         def subscribe(channel)
           start
           @queue << [:subscribe, channel]
           self
         end
 
+        # Unsubscribe a channel from this listener
+        #
+        # @param channel [Channel] the channel to unsubscribe
+        # @return [self]
         def unsubscribe(channel)
           @queue << [:unsubscribe, channel]
           self
         end
 
+        # Start the listener threads for database listening and message dispatching
+        #
+        # @return [self]
         def start
           return if @control
 
@@ -59,13 +97,13 @@ module Sourced
                   @channels.values.each(&:stop)
                 in [:unsubscribe, channel]
                   if @channels.delete(channel.object_id)
-                    @logger.info "#{@info} unsubscribe channel #{channel.object_id}"
+                    @logger.info { "#{@info} unsubscribe channel #{channel.object_id}" }
                     if @channels.empty?
-                      @logger.info "#{@info} all channels unsubscribed."
+                      @logger.info { "#{@info} all channels unsubscribed." }
                     end
                   end
                 in [:subscribe, channel]
-                  @logger.info "#{@info} subscribe channel #{channel.object_id}"
+                  @logger.info { "#{@info} subscribe channel #{channel.object_id}" }
                   @channels[channel.object_id] ||= channel
                 in [:dispatch, message]
                   @channels.values.each { |ch| ch << message }
@@ -85,9 +123,12 @@ module Sourced
             end
           end
 
-          @logger.info "#{@info} Started"
+          @logger.info { "#{@info} Started" }
         end
 
+        # Stop the listener threads and unsubscribe all channels
+        #
+        # @return [self]
         def stop
           @queue << :stop
           @control&.wait
@@ -102,7 +143,10 @@ module Sourced
         end
       end
 
-      # @option db [Sequel::Database]
+      # Initialize a new PubSub instance with database and logger
+      #
+      # @param db [Sequel::Database] the database connection for publishing and listening
+      # @param logger [Logger] the logger instance for recording events
       def initialize(db:, logger:)
         @db = db
         @logger = logger
@@ -110,8 +154,14 @@ module Sourced
         @listeners ||= {}
       end
 
-      # @param channel_name [String]
-      # @return [Channel]
+      # Subscribe to messages on a channel
+      #
+      # Creates or reuses a listener for the given channel name and returns a new channel object
+      # that can be used to receive messages. Multiple subscribers to the same channel share a
+      # single database listener per process.
+      #
+      # @param channel_name [String] the name of the channel to subscribe to
+      # @return [Channel] a new channel object for receiving messages
       def subscribe(channel_name)
         @mutex.synchronize do
           listener = @listeners[channel_name] ||= Listener.new(db: @db, channel_name:, logger: @logger)
@@ -121,8 +171,12 @@ module Sourced
         end
       end
 
-      # @param channel_name [String]
-      # @param event [Sourced::Message]
+      # Publish a message to a channel
+      #
+      # Sends an event to all subscribers on the given channel via Postgres NOTIFY.
+      #
+      # @param channel_name [String] the name of the channel to publish to
+      # @param event [Sourced::Message] the message to publish
       # @return [self]
       def publish(channel_name, event)
         event_data = JSON.dump(event.to_h)
@@ -136,9 +190,10 @@ module Sourced
 
       attr_reader :name
 
-      # @option db [Sequel::Database]
-      # @option name [String]
-      # @option listener [Listener]
+      # Initialize a new channel for receiving messages
+      #
+      # @param name [String] the name of the channel (default: NOTIFY_CHANNEL)
+      # @param listener [Listener] the listener managing this channel
       def initialize(name: NOTIFY_CHANNEL, listener:)
         @name = name
         @running = false
@@ -146,17 +201,23 @@ module Sourced
         @queue = Sourced.config.executor.new_queue
       end
 
+      # Add a message to this channel's queue
+      #
+      # @param message [Sourced::Message] the message to queue
+      # @return [self]
       def <<(message)
         @queue << message
         self
       end
 
-      # Start listening to incoming events
-      # via Postgres LISTEN as implemented in Sequel
+      # Start listening to incoming events on this channel
       #
-      # @option handler [#call, nil] a callable object to use as an event handler
-      # @yieldparam [Sourced::Message]
-      # @yieldparam [Channel]
+      # Blocks until the channel is stopped. Messages are passed to either the provided
+      # handler or the given block.
+      #
+      # @param handler [#call, nil] a callable object to use as an event handler
+      # @yieldparam message [Sourced::Message] the incoming message
+      # @yieldparam channel [Channel] this channel instance
       # @return [self]
       def start(handler: nil, &block)
         return self if @running
@@ -174,7 +235,12 @@ module Sourced
         self
       end
 
-      # Mark the channel to stop on the next tick
+      # Stop listening on this channel
+      #
+      # Unsubscribes from the listener and marks the channel to stop processing messages.
+      # The stop takes effect on the next iteration of the message loop.
+      #
+      # @return [self]
       def stop
         return self unless @running
 
