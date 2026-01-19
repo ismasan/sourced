6eb5501c09d8663e7436f95ba5919f3fdd69812e
Ismael Celis <ismaelct@gmail.com>
2026-01-19 23:08:33 +0000

    Use test module instead of stubbing constants
    Stubbing constants in before blocks throws "constant already defined" warnings.

--- spec/react_dsl_spec.rb
+++ spec/react_dsl_spec.rb
@@ -2,35 +2,35 @@
 
 require 'spec_helper'
 
-RSpec.describe Sourced::React do
-  before do
-    stub_const('TestActor', Class.new(Sourced::Actor) do
-      include Sourced::React
-    end)
+module ReactDSLTests
+  Event = Sourced::Message.define('reactdsl.test')
 
-    stub_const('TestEvent', Sourced::Message.define('test.event'))
+  class Actor < Sourced::Actor
+    include Sourced::React
   end
+end
 
+RSpec.describe Sourced::React do
   it 'raises error when resolving message symbol is not implemented' do
-    expect { TestActor.reaction(nil) }.to raise_error(ArgumentError, /Invalid arguments/)
+    expect { ReactDSLTests::Actor.reaction(nil) }.to raise_error(ArgumentError, /Invalid arguments/)
   end
 
   it 'handles no arguments (wildcard reaction)' do
-    expect { TestActor.reaction {} }.not_to raise_error
+    expect { ReactDSLTests::Actor.reaction {} }.not_to raise_error
   end
 
   it 'handles a single event class' do
-    expect { TestActor.reaction(TestEvent) }
+    expect { ReactDSLTests::Actor.reaction(ReactDSLTests::Event) }
       .not_to raise_error
   end
 
   it 'handles an array of events' do
-    expect { TestActor.reaction(TestEvent, TestEvent) }
+    expect { ReactDSLTests::Actor.reaction(ReactDSLTests::Event, ReactDSLTests::Event) }
       .not_to raise_error
   end
 
   it 'raises an error for an invalid event' do
-    expect { TestActor.reaction(:non_existent_event) }
+    expect { ReactDSLTests::Actor.reaction(:non_existent_event) }
       .to raise_error(ArgumentError, /Cannot resolve message symbol/)
   end
 end
